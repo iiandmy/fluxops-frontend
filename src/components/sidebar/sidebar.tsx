6a26aa0a089307c13834f9a1bc7494c46a5238e7
Alexey Rusel <alexeyrusel@gmail.com>
2025-01-29 12:14:38 +0300

    fix: update header and sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,3 +1,4 @@
+import { IoHomeOutline } from 'react-icons/io5';
 import { Button } from '~/components';
 
 import css from './sidebar.module.css';
@@ -5,16 +6,25 @@ import css from './sidebar.module.css';
 export const Sidebar = () => {
 	return (
 		<aside className={css.root}>
-			<div className={css.logo}>Flux</div>
+			{/* <div className={css.logo}>Flux</div> */}
 			<nav className={css.nav}>
 				<Button variant="primary" className={css.nav_item}>
-					1
+					<div className={css.nav_item_icon}>
+						<IoHomeOutline width={32} height={32} />
+					</div>
+					<p className={css.nav_item_text}>Dashboard</p>
 				</Button>
 				<Button variant="primary" className={css.nav_item}>
-					2
+					<div className={css.nav_item_icon}>
+						<IoHomeOutline width={32} height={32} />
+					</div>
+					<p className={css.nav_item_text}>Dashboard</p>
 				</Button>
 				<Button variant="primary" className={css.nav_item}>
-					3
+					<div className={css.nav_item_icon}>
+						<IoHomeOutline width={32} height={32} />
+					</div>
+					<p className={css.nav_item_text}>Dashboard</p>
 				</Button>
 			</nav>
 		</aside>
