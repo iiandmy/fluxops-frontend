c3034710d141b4760aed5e95f847c3648d9ef33b
Alexey Rusel <alexeyrusel@gmail.com>
2025-02-04 13:08:10 +0300

    feat: add header (#9)

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,28 @@
+import { NavLink } from 'react-router-dom';
+
+import { GenericWrapper } from '../generic-wrapper';
+import { navItems } from './config';
 import css from './header.module.css';
 
 export const Header = () => (
-	<header className={css.root}>
-		<div className={css.container}>
-			<h1>FLUXOPS</h1>
-			<div className={css.placeholder}></div>
-		</div>
-		<div className={css.container}>
-			<div className={css.placeholder}></div>
-			<div className={css.placeholder}></div>
-		</div>
+	<header className={css.wrapper}>
+		<GenericWrapper>
+			<div className={css.container}>
+				<nav className={css.nav}>
+					{navItems.map(({ id, title, href }) => (
+						<NavLink
+							className={({ isActive }) =>
+								[isActive ? css.nav_item_active : '', css.nav_item].join(' ')
+							}
+							key={id}
+							to={href}
+						>
+							{title}
+						</NavLink>
+					))}
+				</nav>
+				<h1>FLUXOPS</h1>
+			</div>
+		</GenericWrapper>
 	</header>
 );
