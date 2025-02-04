17012bf9dac4849f1a539cf1e1cf9bbace288228
Alexey Rusel <alexeyrusel@gmail.com>
2025-02-04 11:04:47 +0300

    chore: delete sidebar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { GenericWrapper } from './generic-wrapper';
 export { Section } from './section';
-export { Sidebar } from './sidebar';
 export { Button } from './button';
 export { Layout } from './layout';
 export { Header } from './header';
