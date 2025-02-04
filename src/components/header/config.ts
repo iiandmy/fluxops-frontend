e59e1df4e3baf3cb3ef04d5c363146f61301d3ac
Alexey Rusel <alexeyrusel@gmail.com>
2025-02-04 13:06:00 +0300

    feat: add header

---
+++ src/components/header/config.ts
@@ -0,0 +1,29 @@
+import { AppRoutes } from '~/constants';
+
+export const navItems = [
+	{
+		id: 1,
+		title: 'Dashboard',
+		href: AppRoutes.Dashboard,
+	},
+	{
+		id: 2,
+		title: 'Clients',
+		href: AppRoutes.Clients,
+	},
+	{
+		id: 3,
+		title: 'Automation',
+		href: AppRoutes.Automation,
+	},
+	{
+		id: 4,
+		title: 'Users',
+		href: AppRoutes.Users,
+	},
+	{
+		id: 5,
+		title: 'Settings',
+		href: AppRoutes.Settings,
+	},
+];
