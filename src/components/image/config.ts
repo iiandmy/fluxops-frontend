9c0e209283522e40c94e3d16ea081e43ca35b0f6
Alexey Rusel <alexeyrusel@gmail.com>
2025-02-08 13:57:18 +0300

    feat: add Image core component

---
+++ src/components/image/config.ts
@@ -0,0 +1,2 @@
+export const LOADING_FALLBACK_URL = 'https://placehold.co/600x400';
+export const ERROR_FALLBACK_URL = 'https://placehold.co/600x400?text=Not+Found';
---
+++ src/components/image/image.tsx
@@ -0,0 +1,67 @@
+import cn from 'classnames';
+import { ComponentProps, forwardRef, useState } from 'react';
+
+import { ERROR_FALLBACK_URL, LOADING_FALLBACK_URL } from './config';
+import css from './image.module.css';
+
+type ObjectFitType = 'none' | 'cover' | 'fill' | 'contain';
+
+interface IImageProps extends ComponentProps<'img'> {
+	fallback?: string;
+	objectFit?: ObjectFitType;
+	rounded?: boolean;
+}
+
+export const Image = forwardRef<HTMLImageElement, IImageProps>(
+	(
+		{
+			src,
+			fallback,
+			className,
+			objectFit = 'fill',
+			rounded = false,
+			...restProps
+		},
+		ref
+	) => {
+		const [isLoading, setIsLoading] = useState(true);
+		const [hasError, setHasError] = useState(false);
+
+		const onErrorHandler = () => {
+			setIsLoading(false);
+			setHasError(true);
+		};
+
+		const onLoadHandler = () => {
+			setIsLoading(false);
+		};
+
+		const computeSrc = () => {
+			if (isLoading || hasError) {
+				return (
+					fallback ?? (hasError ? ERROR_FALLBACK_URL : LOADING_FALLBACK_URL)
+				);
+			}
+		};
+
+		const computedSrc = computeSrc() ?? src;
+
+		const computedClass = cn(
+			css.default,
+			css[`object_fit_${objectFit}`],
+			{ [css.rounded]: rounded },
+			className
+		);
+
+		return (
+			<img
+				ref={ref as never}
+				src={computedSrc}
+				onLoad={onLoadHandler}
+				onError={onErrorHandler}
+				className={computedClass}
+				{...restProps}
+			/>
+		);
+	}
+);
